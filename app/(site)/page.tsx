b10fa2063d9b47560af1d67a8200dc3050c3d4a4
Ashutosh Jha <ashutosh201197@gmail.com>
2023-08-20 15:12:53 +0530

    feat: add header

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
+import { Header } from '@/components/Header';
 
 export default function Home() {
   return (
-    <h1 className='text-green-500'>Main content</h1>
-  )
+    <div className='bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto'>
+      <Header>
+        Main Component
+      </Header>
+    </div>
+    )
 }
